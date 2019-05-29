611bf9c6143aaa0b14467937ba3d0fea980713ec
leventarican <levent@protonmail.com>
2019-05-29 23:08:19 +0200

    implement simple function (ps to kw), screenshot, update README.md

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
+# a simple PS <-> KW calculator
+
 # flutter
-- get flutter SDK
-- set flutter SDK to environment variable PATH: <...>\flutter\bin
-- set android SDK / toolchain to environment variable PATH: <...>\android\sdk\platform-tools
-- visual studio code with flutter extension
+* get flutter SDK
+* set flutter SDK to environment variable PATH: <...>\flutter\bin
+* set android SDK / toolchain to environment variable PATH: <...>\android\sdk\platform-tools
+* visual studio code with flutter extension
     - create hello world flutter with CTRL + SHIFT + P > flutter
-- flutter commands
+* flutter commands
     - flutter doctor    // will find flutter, android toolchain if set properly and android devices
     - flutter devices   // will list devices
+* screenshot with adb
+'''
+adb shell screencap -p /sdcard/screen.png
+adb pull /sdcard/screen.png .
+'''
+![flutter app power[ps/kw]][screen0.png]
--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,115 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lime,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PsKwApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PsKwApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  PsKwState createState() => PsKwState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PsKwState extends State<PsKwApp> {
+  double input;
+  double output;
+  bool psOrKw;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    input = 0.0;
+    output = 0.0;
+    psOrKw = true;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    TextField inputField = TextField(
+      keyboardType: TextInputType.number,
+      onChanged: (str) {
+        try {
+          input = double.parse(str);
+        } catch (e) {
+          input = 0.0;
+        }
+      },
+      decoration: InputDecoration(
+        labelText:
+            "Input a Value in ${psOrKw == false ? "PS" : "KW"}",
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      textAlign: TextAlign.center,
+    );
+
+    AppBar appBar = AppBar(
+      title: Text("Power: PS - KW calculator"),
+    );
+
+    Container unitSwitch = Container(
+      padding: EdgeInsets.all(15.0),
+      child: Row(
+        children: <Widget>[
+          Text("PS"),
+          Radio<bool>(
+              groupValue: psOrKw,
+              value: false,
+              onChanged: (v) {
+                setState(() {
+                  psOrKw = v;
+                });
+              }),
+          Text("KW"),
+          Radio<bool>(
+              groupValue: psOrKw,
+              value: true,
+              onChanged: (v) {
+                setState(() {
+                  psOrKw = v;
+                });
+              }),
+        ],
+      ),
+    );
+
+    Container calcBtn = Container(
+      child: RaisedButton(
+        child: Text("Calculate", style: TextStyle(fontSize: 25)),
+        color: Colors.teal[700].withAlpha(100),
+        onPressed: () {
+          setState(() {
+            psOrKw == false ? output = input / 1.36 : output = input * 1.36;
+          });
+          AlertDialog dialog = AlertDialog(
+            content: psOrKw == false
+                ? Text(
+                    "${input.toStringAsFixed(2)} PS : ${output.toStringAsFixed(2)} KW")
+                : Text(
+                    "${input.toStringAsFixed(2)} KW : ${output.toStringAsFixed(2)} PS"),
+          );
+          showDialog(context: context, child: dialog);
+        },
+      ),
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: Container(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            inputField,
+            unitSwitch,
+            calcBtn,
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
