b92a960bb41721afd02e7e9353cc9288712ddb56
leventarican <levent@protonmail.com>
2020-06-17 23:58:47 +0200

    build more on virtual cockpit

--- car/lib/main.dart
+++ car/lib/main.dart
@@ -7,24 +7,26 @@ void main() => runApp(CarApp());
 class CarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final theme = ThemeData(
-      fontFamily: "Cabin",
-      primaryColor: AppColor.midnightSky,
-      accentColor: AppColor.midnightCloud,
-      primaryTextTheme: Theme.of(context).textTheme.apply(
-            bodyColor: AppColor.textColorDark,
-            displayColor: AppColor.textColorDark,
-          ),
-      textTheme: Theme.of(context).textTheme.apply(
-            bodyColor: AppColor.textColorDark,
-            displayColor: AppColor.textColorDark,
-          ),
-    );
+    // final theme = ThemeData(
+    //   fontFamily: "Cabin",
+    //   primaryColor: AppColor.midnightSky,
+    //   accentColor: AppColor.midnightCloud,
+    //   primaryTextTheme: Theme.of(context).textTheme.apply(
+    //         bodyColor: AppColor.textColorDark,
+    //         displayColor: AppColor.textColorDark,
+    //       ),
+    //   textTheme: Theme.of(context).textTheme.apply(
+    //         bodyColor: AppColor.textColorDark,
+    //         displayColor: AppColor.textColorDark,
+    //       ),
+    // );
 
     return MaterialApp(
-        title: "Virtual Cockpit",
         debugShowCheckedModeBanner: false,
-        theme: theme,
+        theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21),
+        ),
         home: AppPage());
   }
 }
--- car/lib/page.dart
+++ car/lib/page.dart
@@ -1,33 +1,129 @@
 import 'package:car/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:car/circular_percent_indicator.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AppPage extends StatelessWidget {
+const iLabelStyle = TextStyle(
+  fontSize: 22.0,
+  color: Color(0xFF8D8D77),
+);
+
+enum Hybrid { battery, petrol }
+
+class AppPage extends StatefulWidget {
+  @override
+  _AppPageState createState() => _AppPageState();
+}
+
+class _AppPageState extends State<AppPage> {
+  Hybrid selectedPowering;
+  int temp = 19;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Virtual Cockpit"),
+          centerTitle: true,
           backgroundColor: Colors.lightGreen,
         ),
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircularPercentIndicator(
-                  radius: 300.0,
-                  lineWidth: 20.0,
-                  percent: 0.6,
-                  header: Text("Fuel", style: TextStyle(fontSize: 50.0)),
-                  center: Image.asset('assets/gas-station-100.png', scale: 1,),
-                  reverse: true,
-                  backgroundColor: AppColor.twilightCloud,
-                  progressColor: Colors.lightGreen,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: CircularPercentIndicator(
+                    radius: 100.0,
+                    lineWidth: 10.0,
+                    percent: 0.6,
+                    header: Text("Fuel", style: iLabelStyle),
+                    center: Image.asset(
+                      'assets/gas-station-100.png',
+                      scale: 2,
+                      color: Colors.lightGreen.shade100,
+                    ),
+                    reverse: true,
+                    backgroundColor: AppColor.twilightCloud,
+                    progressColor: Colors.lightGreen,
+                  ),
+                ),
+                Expanded(
+                    child: GestureDetector(
+                  child: Container(
+                    child: IconInstrument(
+                      icon: FontAwesomeIcons.batteryHalf,
+                      label: 'battery',
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      if (selectedPowering == Hybrid.petrol) {
+                        selectedPowering = Hybrid.battery;
+                      } else {
+                        selectedPowering = Hybrid.petrol;
+                      }
+                    });
+                    debugPrint('hybrid: $selectedPowering');
+                  },
+                )),
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    inactiveTrackColor: Color(0xFF8D8E98),
+                    activeTrackColor: Colors.white,
+                    thumbColor: Color(0xFFEB1555),
+                    overlayColor: Color(0x29EB1555),
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                  ),
+                  child: Slider(
+                    value: temp.toDouble(),
+                    min: 15.0,
+                    max: 30.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        temp = newValue.round();
+                      });
+                    },
+                  ),
                 ),
               ],
-            ),
-          ),
+            ))
+          ],
         ));
+  }
+}
+
+class IconInstrument extends StatelessWidget {
+  IconInstrument({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 70.0,
+          color: Colors.lightGreen.shade300,
+        ),
+        Text(
+          label,
+          style: iLabelStyle,
+        ),
+        SizedBox(
+          height: 15.0,
+        )
+      ],
+    );
   }
 }
