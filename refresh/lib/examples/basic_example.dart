b27ce6becc81ba51aff984d28e411bf5859aa1c8
leventarican <levent@protonmail.com>
2020-07-13 23:40:53 +0200

    futurebuilder

--- refresh/lib/examples/basic_example.dart
+++ refresh/lib/examples/basic_example.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 /// the flutter basics
 /// 1. create a stateless widget
 /// 2. create a stateful widget: https://github.com/FilledStacks/flutter-tutorials/blob/master/004-flutter-basics/02-setState/lib/home.dart
+/// 3. build view with FutureBuilder: https://github.com/FilledStacks/flutter-tutorials/blob/master/004-flutter-basics/03-futureBuilder/lib/home.dart
 ///
 class BasicExample extends StatelessWidget {
   @override
@@ -18,7 +19,8 @@ class BasicExample extends StatelessWidget {
   }
 }
 
-// 1. stateless
+/// 1. stateless
+/// ############################################################################
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,8 +30,8 @@ class Home extends StatelessWidget {
   }
 }
 
-///
 /// 2. stateful
+/// ############################################################################
 /// show a list of data in a list view
 /// imagine that the data is queried from a database. this will have a bit delay.
 ///
@@ -90,25 +92,68 @@ class _HomeWithState extends State<HomeWith> {
       ),
     );
   }
+}
 
-  ///
-  /// simulate a delay for 2s.
-  /// because of waiting this function is a asynchronous function.
-  /// it will return a list of string elements after 2s delay.
-  ///
-  Future<List<String>> _getListData(
-      {bool hasError = false, bool hasData = true}) async {
-    await Future.delayed(Duration(seconds: 2));
+///
+/// simulate a delay for 2s.
+/// because of waiting this function is a asynchronous function.
+/// it will return a list of string elements after 2s delay.
+///
+Future<List<String>> _getListData(
+    {bool hasError = false, bool hasData = true}) async {
+  await Future.delayed(Duration(seconds: 2));
 
-    if (hasError) {
-      return Future.error('error occured while getting data.');
-    }
-
-    // return empty list if no data queried.
-    if (!hasData) {
-      return List<String>();
-    }
-
-    return List.generate(10, (index) => 'data: $index');
+  if (hasError) {
+    return Future.error('error occured while getting data.');
   }
+
+  // return empty list if no data queried.
+  if (!hasData) {
+    return List<String>();
+  }
+
+  return List.generate(10, (index) => 'data: $index');
+}
+
+/// 3. FutureBuilder
+/// ############################################################################
+/// on button press get data
+/// build list view with FutureBuilder
+
+class HomeFuture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('refresh data');
+          _getListData();
+        },
+      ),
+      backgroundColor: Colors.grey[900],
+      body: FutureBuilder(
+        future: _getListData(),
+        builder: (buildercontext, snapshot) {
+          
+        },
+      ),
+    );
+  }
+}
+
+Widget _getListItemUi(int index, List<String> listItems) {
+  return Container(
+    margin: EdgeInsets.all(5.0),
+    height: 50.0,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0), color: Colors.grey[600]),
+    child: Center(
+      child: Text(
+        listItems[index],
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    ),
+  );
 }
